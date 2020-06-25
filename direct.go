ecd0821a0e0cb66c672a873544a25dd5163caef8
abiao <eabiao@163.com>
2020-06-25 20:49:40 +0800

    代码优化

--- direct.go
+++ direct.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"bufio"
-	"io/ioutil"
 	"log"
-	"os"
-	"sort"
+	"net/http"
 	"strings"
 	"sync"
 )
@@ -17,30 +15,29 @@ var (
 
 // 直连
 type Direct struct {
-	defaultMap map[string]bool
-	recordMap  map[string]bool
+	hostMap map[string]bool
 }
 
 // 初始化
 func initDirect() *Direct {
 	d := &Direct{
-		defaultMap: make(map[string]bool),
-		recordMap:  make(map[string]bool),
+		hostMap: make(map[string]bool),
 	}
 	d.loadDirect()
-	d.saveDirect()
 	return d
 }
 
 // 加载文件
 func (d *Direct) loadDirect() {
-	file, err := os.Open("./direct.txt")
+
+	resp, err := http.Get("http://lvzhanbiao.cn:3000/sidu/autoproxy/getAll")
 	if err != nil {
 		return
 	}
-	defer file.Close()
 
-	br := bufio.NewReader(file)
+	defer resp.Body.Close()
+
+	br := bufio.NewReader(resp.Body)
 	for {
 		lineData, _, err := br.ReadLine()
 		if err != nil {
@@ -52,23 +49,13 @@ func (d *Direct) loadDirect() {
 			continue
 		}
 
-		if strings.HasPrefix(host, "||") {
-			host = strings.TrimPrefix(host, "||")
-			d.defaultMap[host] = true
-		} else if !d.isDefaultDirect(host) {
-			d.recordMap[host] = true
-		}
+		d.hostMap[host] = true
 	}
 }
 
 // 判断是否为直连
 func (d *Direct) isDirect(host string) bool {
-	return d.isDefaultDirect(host) || d.recordMap[host]
-}
-
-// 判断是否为默认直连
-func (d *Direct) isDefaultDirect(host string) bool {
-	for direct := range d.defaultMap {
+	for direct := range d.hostMap {
 		if strings.HasSuffix(host, direct) {
 			return true
 		}
@@ -85,31 +72,17 @@ func (d *Direct) addDirect(host string) {
 		return
 	}
 
-	log.Println("add", host)
 	domain := getTopDomain(host)
-	if domain != "" {
-		d.defaultMap[domain] = true
-	} else {
-		d.recordMap[host] = true
+	if domain == "" {
+		return
 	}
 
-	d.saveDirect()
+	d.hostMap[domain] = true
+	d.putDirect(domain)
 }
 
 // 保存
-func (d *Direct) saveDirect() {
-	var defaultList []string
-	for k := range d.defaultMap {
-		defaultList = append(defaultList, "||"+k)
-	}
-	sort.Strings(defaultList)
-
-	var recordList []string
-	for k := range d.recordMap {
-		recordList = append(recordList, k)
-	}
-	sort.Strings(recordList)
-
-	directText := strings.Join(defaultList, "\n") + "\n" + strings.Join(recordList, "\n")
-	ioutil.WriteFile("./direct.txt", []byte(directText), os.ModePerm)
+func (d *Direct) putDirect(domain string) {
+	log.Println("add", domain)
+	http.Get("http://lvzhanbiao.cn:3000/sidu/autoproxy/putHost?host=" + domain)
 }
