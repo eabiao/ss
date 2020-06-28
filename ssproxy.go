50dd5b3a514ed20db6981ec83cf450ee273e5398
abiao <eabiao@163.com>
2020-06-28 11:05:32 +0800

    升级go-ss连接为go-ss2连接

--- ssproxy.go
+++ ssproxy.go
@@ -51,30 +51,3 @@ func (sp *SSProxy) connect(addr string) (net.Conn, error) {
 
 	return rc, nil
 }
-
-//type SSProxy struct {
-//	server string
-//	cipher *shadowsocks.Cipher
-//}
-//
-//// 初始化ss代理
-//func initSSProxy() *SSProxy {
-//	return &SSProxy{
-//		server: config.Server,
-//		cipher: initCipher(config.Method, config.Passwd),
-//	}
-//}
-//
-//// 初始化加密器
-//func initCipher(method, passwd string) *shadowsocks.Cipher {
-//	cipher, err := shadowsocks.NewCipher(method, passwd)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	return cipher
-//}
-//
-//// ss代理连接
-//func (sp *SSProxy) connect(addr string) (net.Conn, error) {
-//	return shadowsocks.Dial(addr, sp.server, sp.cipher.Copy())
-//}
