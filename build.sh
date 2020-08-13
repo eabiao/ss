bde001822b11a29c0bd70b7b7fa957be879b6f1b
abiao <eabiao@163.com>
2020-08-13 16:42:59 +0800

    代码优化

--- build.sh
+++ build.sh
@@ -5,7 +5,14 @@ APP_NAME=ss
 GO=/mnt/d/soft/go/bin/go.exe
 UPX=/mnt/d/soft/upx-3.96-win64/upx.exe
 
-rm -rf bin/*.exe
+$GO generate
+
+go get github.com/akavel/rsrc
+rsrc.exe -manifest manifest.txt -ico plane.ico -o main.syso
+rm -f manifest.txt
+
+rm -f bin/*.exe
 $GO build -ldflags "-s -w" -o bin/tmp.exe
 $UPX -9 -o bin/${APP_NAME}.exe bin/tmp.exe
 rm -f bin/tmp.exe
+rm -f main.syso
---
+++ icon_gen.go
@@ -0,0 +1,43 @@
+// +build generate
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+//go:generate go run icon_gen.go
+func main() {
+	genIconManifest("manifest.txt")
+}
+
+func genIconManifest(file string) {
+	w, err := os.Create(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer w.Close()
+	fmt.Fprintln(w, `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<assemblyIdentity
+    version="1.0.0.0"
+    processorArchitecture="x86"
+    name="controls"
+    type="win32"
+></assemblyIdentity>
+<dependency>
+    <dependentAssembly>
+        <assemblyIdentity
+            type="win32"
+            name="Microsoft.Windows.Common-Controls"
+            version="6.0.0.0"
+            processorArchitecture="*"
+            publicKeyToken="6595b64144ccf1df"
+            language="*"
+        ></assemblyIdentity>
+    </dependentAssembly>
+</dependency>
+</assembly>`)
+}
